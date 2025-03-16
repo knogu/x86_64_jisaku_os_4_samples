0f3b6bb64f81db21df023411656275d0ef779902
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2025-03-16 19:12:10 +0900

    try to detect arp

--- A01_syscall/apps/06_beef_server/app.c
+++ A01_syscall/apps/06_beef_server/app.c
@@ -4,6 +4,8 @@
 
 void dump_frame(unsigned char buf[], unsigned short len);
 
+int is_arp(unsigned char buf[], unsigned short len);
+
 struct ether_hdr {
     char dst_addr[6];
     char src_addr[6];
@@ -19,6 +21,9 @@ int main(void)
 
 		/* 受信 */
 		while (!(len = receive_frame(buf)));
+		if (is_arp(buf, len)) {
+			while(1) {}
+		}
 		dump_frame(buf, len);
 		puts("__RECEIVED\r\n");
 
@@ -42,9 +47,6 @@ void dump_frame(unsigned char buf[], unsigned short len)
 {
 	unsigned short i;
 	for (i = 0; i < len; i++) {
-		if (buf[i] == 0xff) {
-			puts("FF FOUND");
-		}
 		puth(buf[i], 2);
 		putc(' ');
 
@@ -59,4 +61,59 @@ void dump_frame(unsigned char buf[], unsigned short len)
 
 	if (len > 0)
 		puts("\r\n");
+}
+
+void memcpy(void *dst, void *src, unsigned long long size)
+{
+	unsigned char *d = (unsigned char *)dst;
+	unsigned char *s = (unsigned char *)src;
+	for (; size > 0; size--)
+		*d++ = *s++;
+}
+
+int strncmp(char *s1, char *s2, unsigned long long n)
+{
+	char is_equal = 1;
+
+	for (; (*s1 != '\0') && (*s2 != '\0'); s1++, s2++, n--) {
+		if (*s1 != *s2 || n == 1) {
+			is_equal = 0;
+			break;
+		}
+	}
+
+	if (is_equal) {
+		if (*s1 != '\0') {
+			return 1;
+		} else if (*s2 != '\0') {
+			return -1;
+		} else {
+			return 0;
+		}
+	} else {
+		return (int)(*s1 - *s2);
+	}
+}
+
+short conv_endian16(short value) {
+    short out;
+    out = value >> 8;
+    out |= value << 8;
+    return out;
+}
+
+int is_arp(unsigned char buf[], unsigned short len)
+{
+	struct ether_hdr hdr;
+	memcpy(&hdr, buf, sizeof(struct ether_hdr));
+	char pat[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+	if (strncmp(hdr.dst_addr, pat, 6)) {
+		puts("BROADCAST FOUND. SRC: ");
+		puts(hdr.src_addr);
+		if (conv_endian16(hdr.type) == 0x0806) {
+			puts("TYPE IS ARP");
+			return 1;
+		}
+	}
+	return 0;
 }
